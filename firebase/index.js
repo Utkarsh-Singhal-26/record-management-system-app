6db9fb4572d742a789a3841caf6a6152aec7203f
Utkarsh Singhal <118098216+Utkarsh-Singhal-26@users.noreply.github.com>
2024-07-20 17:30:03 +0000

    [ADDED : Firebase]

---
+++ firebase/index.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_apiKey,
+  authDomain: process.env.EXPO_PUBLIC_authDomain,
+  projectId: process.env.EXPO_PUBLIC_projectId,
+  storageBucket: process.env.EXPO_PUBLIC_storageBucket,
+  messagingSenderId: process.env.EXPO_PUBLIC_messagingSenderId,
+  appId: process.env.EXPO_PUBLIC_appId,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const firestore = getFirestore(app);
